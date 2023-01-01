fe0e7a875d7dc3bb77d7dfdffce367aa5f6abc60
Brian Falaz <brianfalasz@gmail.com>
2023-01-01 14:30:07 -0600

    added a cool little search feature to the project in the road to react book. Don't think this is part of the standard project in the book. This is just something i've implemented on my own.

--- Fundamentals/hacker-stories/src/App.jsx
+++ Fundamentals/hacker-stories/src/App.jsx
@@ -1,9 +1,10 @@
-import { useState } from 'react'
+import React from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [searchTerm, useSearchTerm] = React.useState('');
 
   
   const list = [
@@ -24,30 +25,63 @@ function App() {
     }
   ]
 
+  const handleChange = (e) => {
+    e.preventDefault();
+
+    useSearchTerm(e.target.value);
+    list
+  }
+
   return (
     <div className="App">
       <div>
         <h1>My Hacker Stories</h1>
-
-        <ul>
-          {list.map(item => {
-            return (
-              <li key={item.objectID}>
-                <a href={item.url}>{item.title}</a>
-                <p>Author(s): {item.author}</p>
-                <p>Comments: {item.num_comments}</p>
-                <p>Points: {item.points}</p>
-
-              </li>
-
-            );
-          })}
-        </ul>
+        <Search handleSearch={handleChange} term={searchTerm} />
+        <List list={list} term={searchTerm} />
       </div>
-      <label htmlFor="search">Search:</label>
-      <input id="search" type="text" />
     </div>
   )
+}
+
+const List = ({ list, term }) => {
+
+
+  return (
+    <ul>
+    {list.map(item => {
+      if(term && Object.values(item).join(' ').includes(term)){
+        return <Item key={item.objectID} item={item} />
+      }
+    })}
+    </ul>
+  )
+
+}
+
+
+const Item = ({item}) => {
+   
+  return (
+    <li style={{textAlign: 'left'}}>
+      <a href={item.url}>{item.title}</a>
+      <span> Author(s): {item.author}</span>
+      <span> Comments: {item.num_comments}</span>
+      <span> Points: {item.points}</span>
+    </li>
+  );
+}
+
+const Search = ({handleSearch, term}) => {
+
+  
+  return (
+    <>
+      <label htmlFor="search">Search:</label>
+      <input value={term} id="search" type="text" onChange={handleSearch} />
+      <p><strong>The current search term is:</strong> {term}</p>
+    </>
+  );
+
 }
 
 export default App
